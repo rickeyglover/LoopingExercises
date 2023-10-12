92ae3466fbf9d62b4651bda854ea4d39bae144c5
rickeyglover <rickeygloverw@gmail.com>
2023-10-12 11:51:53 -0400

    first commit

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 4) {
+            i++;
+            System.out.println("I love Javier");
+        }
+    }
+}
